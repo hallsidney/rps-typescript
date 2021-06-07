0e1c6e18f05c9fa60b23be978749168c3b8301e5
SIDNEY <73758696+hallsidney@users.noreply.github.com>
2021-06-07 16:08:32 -0500

    Completed RPS Kata

--- src/__test__/rps.test.js
+++ src/__test__/rps.test.js
@@ -4,6 +4,39 @@
 
 const rps = require("../rps");
 
+//Tests for RPS moves, in both orders
 it('rock beats scissors', () =>{
     expect(rps("rock", "scissors")).toBe("rock");
 });
+it('rock beats scissors', () =>{
+    expect(rps("scissors", "rock")).toBe("rock");
+});
+
+it('scissors best paper', () =>{
+    expect(rps("scissors", "paper")).toBe("scissors");
+});
+it('scissors best paper', () =>{
+    expect(rps("paper", "scissors")).toBe("scissors");
+});
+
+it('paper beats rock', () => {
+    expect(rps("paper", "rock")).toBe("paper");
+})
+it('paper beats rock', () => {
+    expect(rps("rock", "paper")).toBe("paper");
+})
+
+
+//Tests for Draws
+it('both choose rock', () =>{
+    expect(rps("rock", "rock")).toBe("draw");
+})
+
+it('both choose paper', () =>{
+    expect(rps("paper", "paper")).toBe("draw");
+})
+
+it('both choose scissors', () =>{
+    expect(rps("scissors'", "scissors'")).toBe("draw");
+})
+
--- src/rps.js
+++ src/rps.js
@@ -1,7 +1,23 @@
 // console.log("Typescript is dooooppppeee");
 
 const rps = (p1, p2) => {
-    return "";
+
+    if(p1 === p2)
+        return "draw"
+    //Step2 Build conditions so that both tests pass
+    let arr = [p1,p2]
+    if(!arr.includes("paper")) //Same as rock and scissors
+        return "rock";
+    else if (arr.includes("scissors")) // implies paper and scissors
+        return "scissors";
+    else
+        return "paper" // last option paper and rock
+
+    //return "scissors" //Step 1
+
 };
 
+
 module.exports = rps;
+
+//STEP 1: Make test pass in most simple way possible
